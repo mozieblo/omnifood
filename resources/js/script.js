9ba9d2169d5e105e2a98d7592db1fdf444d07e08
mozieblo <m.ozieblo@wp.pl>
2019-07-18 12:39:57 +0200

    animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,8 @@
 /* STICKY NAVIGATION */
 
 window.onscroll = () => {
-    stickyNav()
+    stickyNav();
+    animatedEffect();
 };
 
 function stickyNav() {
@@ -55,4 +56,36 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+/* ANIMATION ON SCROLL */
+
+function animatedEffect() {
+    const jsWp1 = document.querySelector('.js--wp-1');
+    const animate = document.querySelector('.section-features').offsetTop;
+    const jsWp2 = document.querySelector('.js--wp-2');
+    const animate2 = document.querySelector('.js--section-cities').offsetTop;
+    const jsWp3 = document.querySelector('.js--wp-3');
+    const animate3 = document.querySelector('.js--section-steps').offsetTop;
+    const jsWp4 = document.querySelector('.js--wp-4');
+    const animate4 = document.querySelector('.js--section-plans').offsetTop;
+    
+    if (window.pageYOffset >= animate) {
+        jsWp1.classList.add('animated');
+        jsWp1.classList.add('fadeIn');
+    }
+     
+    if (window.pageYOffset >= animate2) {
+        jsWp2.classList.add('animated');
+        jsWp2.classList.add('fadeIn');
+    }
+    
+    if (window.pageYOffset >= animate3) {
+        jsWp3.classList.add('animated');
+        jsWp3.classList.add('fadeInUp');
+    }
+    
+    if (window.pageYOffset >= animate4) {
+        jsWp4.classList.add('animated');
+        jsWp4.classList.add('pulse');
+    }
+}
 
